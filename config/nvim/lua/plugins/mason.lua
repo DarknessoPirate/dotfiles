d16663df3337ddd1aa7bd13845dd7fcacaa72729
darknesso <darknesso@debian>
2025-07-01 16:35:29 +0200

    AstroVim config added

---
+++ config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,24 @@
+-- Customize Mason
+
+---@type LazySpec
+return {
+  -- use mason-tool-installer for automatically installing Mason packages
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    -- overrides `require("mason-tool-installer").setup(...)`
+    opts = {
+      -- Make sure to use the names found in `:Mason`
+      ensure_installed = {
+        -- install language servers
+        "lua-language-server",
+        "omnisharp",
+        "netcoredbg",
+        -- install formatters
+        "stylua",
+
+        -- install any other package
+        "tree-sitter-cli",
+      },
+    },
+  },
+}
